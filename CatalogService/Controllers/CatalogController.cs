6afd885a19cc6601017df17e04b155a74709f9ee
Nishant Kumar Sharma <nishant.sharma88@hotmail.com>
2020-12-25 20:21:41 +0530

    first commit

---
+++ CatalogService/Controllers/CatalogController.cs
@@ -0,0 +1,154 @@
+﻿using CatalogService.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CatalogService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public CatalogController(DatabaseContext context)
+        {
+
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>>
+            GetProducts()
+        {
+
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(int id, Product product)
+        {
+
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+
+
+            }
+            _context.Entry(product).State = EntityState.Modified;
+            try
+            {
+
+                await _context.SaveChangesAsync();
+
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+
+                if (!ProductExists(id))
+                {
+
+                    return NotFound();
+
+                }
+                else
+                {
+
+                    throw;
+                }
+
+
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProduct", new { id = product.ProductId }, product);
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+
+                return NotFound();
+
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+
+        }
+        // GET: api/<CatalogController>
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/<CatalogController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<CatalogController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<CatalogController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<CatalogController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ CatalogService/Migrations/20201225023441_initial.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CatalogService.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    categoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.categoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    categoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_categoryId",
+                        column: x => x.categoryId,
+                        principalTable: "Categories",
+                        principalColumn: "categoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_categoryId",
+                table: "Products",
+                column: "categoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
